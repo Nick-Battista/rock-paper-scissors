559fbd641cb386de93c861d75bfcee73e7690d54
Nick Battista <113464099+Nick-Battista@users.noreply.github.com>
2025-09-24 16:02:36 -0400

    feat: add playRound functionality to game
    Add playRound functionality to the game for one single round.

--- script.js
+++ script.js
@@ -21,8 +21,43 @@ function getHumanChoice() {
     return playerChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase()
+
+    if (humanChoice == computerChoice) {
+        console.log("It's a DRAW! Well played!");
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You LOSE! Paper beats Rock!");
+        computerScore += 1;
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You WIN! Rock beats Scissors!");
+        humanScore += 1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You WIN! Paper beats Rock!");
+        humanScore += 1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You LOSE! Scissors beats Paper!");
+        computerScore += 1;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You LOSE! Rock beats Scissors");
+        computerScore += 1;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You WIN! Scissors beats Paper!");
+        humanScore += 1;
+    }
+}
+
 let computer = getComputerChoice()
 let player = getHumanChoice()
 
-console.log(computer);
-console.log(player);
+console.log(`The computer threw out: ${computer}`);
+console.log(`You threw out: ${player}`);
+
+playRound(player, computer);
