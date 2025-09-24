9b9376417dcca5bb62e71abea4ca201e6d63e2cf
Nick Battista <113464099+Nick-Battista@users.noreply.github.com>
2025-09-24 15:22:06 -0400

    feat: add player function
    Add player function for guessing one of the options.

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ function getComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
     let move = "";
     if (num == 1) {
-        move = "Rock";
+        move = "rock";
     }
     else if (num == 2) {
-        move = "Paper";
+        move = "paper";
     }
     else if (num == 3) {
-        move = "Scissors";
+        move = "scissors";
     }
     return move;
 }
 
-let result = getComputerChoice()
-console.log(result);
+function getHumanChoice() {
+    let playerChoice = prompt("Enter in rock, paper, or scissors...");
+    return playerChoice;
+}
+
+let computer = getComputerChoice()
+let player = getHumanChoice()
+
+console.log(computer);
+console.log(player);
